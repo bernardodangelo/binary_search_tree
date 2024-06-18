9dae03bb089399660e838644c178e95f7eec9c64
Bernardo D'Angelo <bernardodangelo8@gmail.com>
2024-06-17 21:42:59 -0300

    initial commit

---
+++ src/arvore/NoArv.java
@@ -0,0 +1,39 @@
+package arvore;
+
+import dados.Item;
+
+public class NoArv {
+	private Item info; // o tipo Item est� declarado no cap�tulo 1
+	private NoArv esq, dir;
+
+	public NoArv(Item elem) {
+		this.info = elem;
+		this.esq = null;
+		this.dir = null;
+	}
+
+	public NoArv getEsq() {
+		return this.esq;
+	}
+
+	public NoArv getDir() {
+		return this.dir;
+	}
+
+	public Item getInfo() {
+		return this.info;
+	}
+
+	public void setEsq(NoArv no) {
+		this.esq = no;
+	}
+
+	public void setDir(NoArv no) {
+		this.dir = no;
+	}
+
+	public void setInfo(Item elem) {
+		this.info = elem;
+	}
+
+}
